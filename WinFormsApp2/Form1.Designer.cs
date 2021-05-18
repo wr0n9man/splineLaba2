d8b97de658b2658bcf11ebb0cdbbf5f42a18aadf
wr0n9man <rafigov2017@yandex.ru>
2021-05-18 14:53:29 +0400

    laba2

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(682, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "E";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(348, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 400);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(701, 72);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 23);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(701, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        int x1, y1, x2, y2;
+        private Bitmap bmp;
+        private Bitmap bmp2;
+        private Pen blackPen;
+        private Pen greenPen;
+        List<Point> points = new List<Point>();
+        List<Point> points2 = new List<Point>();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp2 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            blackPen = new Pen(Color.Blue, 1);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x2 = Convert.ToInt32(e.X); // координата по оси X
+            y2 = Convert.ToInt32(e.Y);
+
+        }
+
+        private void reset()
+        {
+            //x2 = Convert.ToInt32(e.X); // координата по оси X
+            //y2 = Convert.ToInt32(e.Y); // координата по оси Y
+            points.Add(new Point(x2, y2));
+            this.Invalidate();
+
+            if ((x1 + y1) == 0)
+            {
+               
+                x1 = x2;
+                y1 = y2;
+            }
+            else
+            {
+                Graphics g = Graphics.FromImage(bmp);
+
+                blackPen = new Pen(Brushes.Red, 2);
+                g.DrawLine(blackPen, x1, y1, x2, y2);
+
+                blackPen.Dispose();
+                g.Dispose();
+                pictureBox1.Image = bmp;
+                pictureBox1.Invalidate();
+
+                x1 = x2;
+                y1 = y2;
+            }
+          
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+     
+            greenPen = new Pen(Brushes.Green, 2);
+            Graphics br = Graphics.FromImage(bmp2);
+            br.Clear(Color.White);
+            points2 = DouglasPeuckerReduction(points, Convert.ToDouble( numericUpDown1.Value));
+            for (int i = 1; i < points2.Count; i++)
+            {
+                br.DrawLine(greenPen, points2[i - 1], points2[i]);
+                
+            }
+            pictureBox2.Image = bmp2;
+            pictureBox2.Invalidate();
+
+        }
+
+ 
+        public static List<Point> DouglasPeuckerReduction (List<Point> Points, Double Tolerance)
+        {
+            if (Points == null || Points.Count < 3)
+                return Points;
+
+            Int32 firstPoint = 0;
+            Int32 lastPoint = Points.Count - 1;
+            List<Int32> pointIndexsToKeep = new List<Int32>();
+
+            pointIndexsToKeep.Add(firstPoint);
+            pointIndexsToKeep.Add(lastPoint);
+
+            while (Points[firstPoint].Equals(Points[lastPoint]))
+            {
+                lastPoint--;
+            }
+
+            DouglasPeuckerReduction(Points, firstPoint, lastPoint,  Tolerance, ref pointIndexsToKeep);
+
+            List<Point> returnPoints = new List<Point>();
+            pointIndexsToKeep.Sort();
+            foreach (Int32 index in pointIndexsToKeep)
+            {
+                returnPoints.Add(Points[index]);
+            }
+
+            return returnPoints;
+        }
+
+        private static void DouglasPeuckerReduction(List<Point>points, Int32 firstPoint, Int32 lastPoint, Double tolerance, ref List<Int32> pointIndexsToKeep)
+        {
+            Double maxDistance = 0;
+            Int32 indexFarthest = 0;
+
+            for (Int32 index = firstPoint; index < lastPoint; index++)
+            {
+                Double distance = PerpendicularDistance
+                    (points[firstPoint], points[lastPoint], points[index]);
+                if (distance > maxDistance)
+                {
+                    maxDistance = distance;
+                    indexFarthest = index;
+                }
+            }
+
+            if (maxDistance > tolerance && indexFarthest != 0)
+            {
+                pointIndexsToKeep.Add(indexFarthest);
+
+                DouglasPeuckerReduction(points, firstPoint,
+                indexFarthest, tolerance, ref pointIndexsToKeep);
+                DouglasPeuckerReduction(points, indexFarthest,
+                lastPoint, tolerance, ref pointIndexsToKeep);
+            }
+        }
+
+
+        public static Double PerpendicularDistance (Point Point1, Point Point2, Point Point)
+        {
+           
+
+            Double area = Math.Abs(.5 * (Point1.X * Point2.Y + Point2.X *
+            Point.Y + Point.X * Point1.Y - Point2.X * Point1.Y - Point.X *
+            Point2.Y - Point1.X * Point.Y));
+            Double bottom = Math.Sqrt(Math.Pow(Point1.X - Point2.X, 2) +
+            Math.Pow(Point1.Y - Point2.Y, 2));
+            Double height = area / bottom * 2;
+
+            return height;
+
+      
+        }
+
+    }
+}
